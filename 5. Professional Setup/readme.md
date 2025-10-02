5998686c1852e9ea22f979c0f77ecc65e396bfd9
Mayank Bajaj <mayankbajaj672@gmail.com>
2025-10-02 10:12:42 +0530

    user and video model

--- 5. Professional Setup/readme.md
+++ 5. Professional Setup/readme.md
@@ -1,1 +1,3 @@
 when connecting database , always use try catch and async await
+
+async method jab bhi complete hota hai vo apko promise return karta hai
--- 5. Professional Setup/src/index.js
+++ 5. Professional Setup/src/index.js
@@ -4,11 +4,57 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Error in Express app");
+      throw err;
+    })
 
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MonogoDB connection error:", err);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 /*
 import express from "express";
 const app = express();
